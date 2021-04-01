127d096835da872b2fca5c03d5df03b65d816423
demiurg <john-alp@mail.ru>
2021-04-01 12:07:38 +0300

    add aop

--- src/main/java/com/demiurg/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
+++ src/main/java/com/demiurg/spring/mvc_hibernate_aop/aspect/MyLoggingAspect.java
@@ -1,4 +1,29 @@
 package com.demiurg.spring.mvc_hibernate_aop.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
 public class MyLoggingAspect {
+
+    // (* return type, методы какого класса, * на все классы этого пакета, * на все методы всех классов
+    // , .. для методов с любым колличеством параметров)в скобках пишем на какие методы будет распространяться этот advice
+    @Around("execution(* com.demiurg.spring.mvc_hibernate_aop.dao.*.*(..))")
+    public Object aroundAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+        String methodName = methodSignature.getName();
+
+        System.out.println("Begin of " + methodName);
+
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+
+        System.out.println("End of " + methodName);
+
+        return targetMethodResult;
+    }
+
 }
